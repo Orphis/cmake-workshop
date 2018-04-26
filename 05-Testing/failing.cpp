94f70de0a7f72996fb7a6bcc9d3c81d8551904cc
Florent Castelli <florent.castelli@gmail.com>
2018-04-26 11:10:26 +0200

    Add testing exercise

---
+++ 05-Testing/failing.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "All tests failed!" << std::endl;
+
+  return 1;
+}
---
+++ 05-Testing/passing.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "All tests passed!" << std::endl;
+
+  return 0;
+}
